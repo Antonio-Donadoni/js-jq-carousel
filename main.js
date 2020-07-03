b7d3717e3117ab971e00a60b5236de481d66670e
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-07-03 15:16:31 +0200

    JQ CAROUSEL

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+
+$(".next").click(goNext);
+$(".prev").click(goPrev);
+
+});
+
+
+
+
+// FUNZIONI
+
+function goNext() {
+  var currentImg= $("img.active");
+  var nextImg = $("img.active").next();
+
+  var currentIcon= $("i.active");
+  var nextIcon = $("i.active").next();
+
+  currentImg.removeClass("active");
+  currentIcon.removeClass("active")
+
+  if (currentImg.hasClass("last")) {
+    $("img.first").addClass("active");
+    $("i.first").addClass("active");
+  } else
+  nextImg.addClass("active");
+  nextIcon.addClass("active");
+}
+
+function goPrev() {
+  var currentImg= $("img.active");
+  var prevImg = $("img.active").prev();
+
+  var currentIcon= $("i.active");
+  var prevIcon = $("i.active").prev();
+
+  currentImg.removeClass("active");
+  currentIcon.removeClass("active")
+
+  if (currentImg.hasClass("first")) {
+    $("img.last").addClass("active");
+    $("i.last").addClass("active");
+  } else
+  prevImg.addClass("active");
+  prevIcon.addClass("active");
+}
